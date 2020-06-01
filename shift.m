2f1ce785711643ec196512dd982835f2e1fe779a
Jason Nan <janan@ucsd.edu>
2020-06-01 15:02:08 -0700

    Finished

--- shift.m
+++ shift.m
@@ -1,4 +1,5 @@
-function [shift_data,fa_test]=shift(index,train_trial)
+function [shift_data,pp_test]=shift(index,train_trial,Xplan)
+    % p_test is our testing set
     K=8;D=97;L=length(index);inc=15;rate=floor(150/15);N=rate*L;
     train_hold=zeros(D,200,N,K);
     for k=1:K
@@ -11,9 +12,19 @@ function [shift_data,fa_test]=shift(index,train_trial)
 
     spike_count=squeeze(sum(train_hold,2));
     combined_count=reshape(spike_count,D,8*N)';
-    rng(0)
-    [lambda,psi,T,stats,F] = factoran(combined_count,3);
-    shift_data=spike_count*lambda;
     
+    train_labels=ones(8,N).*[1:8]';
+    train_labels=reshape(train_labels',N*8,1);
+    
+    test_labels=ones(8,91).*[1:8]';
+    test_labels=reshape(test_labels',728,1);
+    
+    rng(0)
+    [coeff,score,pcvar,mu,v,S] = ppca(combined_count,3);
+    shift_data=combined_count*coeff;
+    shift_data(:,end+1)=train_labels;
+    
+    pp_test=Xplan*coeff;
+    pp_test(:,end+1)=test_labels;
 end
 
