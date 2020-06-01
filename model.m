d553bd1a9d0aac27a533c7078cbf68a94659c292
Jason Nan <janan@ucsd.edu>
2020-06-01 11:39:44 -0700

    Model function complete

--- model.m
+++ model.m
@@ -1,18 +1,12 @@
 function accuracy=model(training_set, testing_set)
     rng(0) % comment out the rng seed so we can get true cross-validation
     
-    training_input=training_set(:,1:end-1);
-    training_output=trainig_set(:,end);
+    train_input=prepare_input_dataset(training_set(:,1:end-1));
+    train_output=prepare_output_dataset(trainig_set(:,end));
     
-    testing_input=testing_set(:,1:end-1);
-    testing_output=testing_set(:,end);
+    test_input=prepare_input_dataset(testing_set(:,1:end-1));
+    test_output=prepare_output_dataset(testing_set(:,end));
     
-    % Change for different type of data
-    data_input=
-    [data_input]=prepare_input_dataset(data_input);
-
-    data_output=
-    [data_output]=prepare_output_dataset(data_output);
 
     % Initialize network
     n_hidden = 2;
@@ -20,12 +14,6 @@ function accuracy=model(training_set, testing_set)
     n_outputs=size(data_output,2);
     net=initialize_network(n_inputs,n_hidden,n_outputs);
 
-    % Format data into test/train input/output
-    train_input=data_input(train_values,:);
-    train_output=data_output(train_values,:);
-
-    test_input=data_input(test_values,:);
-    test_output=data_output(test_values,:);
 
     % Set training parameters
     l_rate = 0.3; n_epoch = 200;
