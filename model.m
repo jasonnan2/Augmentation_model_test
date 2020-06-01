e50c509e46aa6cb6339c993db9bd381d8507ad79
Jason Nan <janan@ucsd.edu>
2020-06-01 11:00:01 -0700

    Training and testing of the model

---
+++ model.m
@@ -0,0 +1,35 @@
+function accuracy=model(data_input,data_output)
+    rng(0) % comment out the rng seed so we can get true cross-validation
+
+    % Change for different type of data
+    data_input=
+    [data_input]=prepare_input_dataset(data_input);
+
+    data_output=
+    [data_output]=prepare_output_dataset(data_output);
+
+    % Initialize network
+    n_hidden = 2;
+    n_inputs=size(data_input,2);
+    n_outputs=size(data_output,2);
+    net=initialize_network(n_inputs,n_hidden,n_outputs);
+
+    % Format data into test/train input/output
+    train_input=data_input(train_values,:);
+    train_output=data_output(train_values,:);
+
+    test_input=data_input(test_values,:);
+    test_output=data_output(test_values,:);
+
+    % Set training parameters
+    l_rate = 0.3; n_epoch = 200;
+    [net] = train_network(net, train_input, train_output, l_rate, n_epoch);
+
+    for j=1:size(test_output,1)
+        true_val(j)=find(test_output(j,:)==max(test_output(j,:)));
+    end
+    [test]=predict(net, test_input);
+
+    correct=numel(find(test==true_val));
+    accuracy=correct/length(test);
+end
