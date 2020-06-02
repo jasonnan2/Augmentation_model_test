1c79ddae6fd1a48648b81de123f7142cb0ba7acb
Jason Nan <janan@ucsd.edu>
2020-06-01 17:21:50 -0700

    Wrote everything in terms of variables

--- shift.m
+++ shift.m
@@ -1,6 +1,6 @@
 function [clean_data,shift_data,pp_test]=shift(index,train_trial,Xplan)
     % p_test is our testing set
-    K=8;D=97;L=length(index);inc=15;rate=floor(150/15);N=rate*L;
+    K=4;D=97;L=length(index);inc=15;rate=floor(150/15);N=rate*L;
     train_hold=zeros(D,200,N,K);
     clean_hold=zeros(D,200,L,K);
     for k=1:K
@@ -14,21 +14,21 @@ function [clean_data,shift_data,pp_test]=shift(index,train_trial,Xplan)
 
     % Baseline no data augmentation dataset
     clean_count=squeeze(sum(clean_hold,2));
-    clean_combined=reshape(clean_count,D,8*L)';
+    clean_combined=reshape(clean_count,D,K*L)';
     
     % With data augmentation
     spike_count=squeeze(sum(train_hold,2));
-    combined_count=reshape(spike_count,D,8*N)';
+    combined_count=reshape(spike_count,D,K*N)';
     
     % labels
-    train_labels=ones(8,N).*[1:8]';
-    train_labels=reshape(train_labels',N*8,1);
+    train_labels=ones(K,N).*[1:K]';
+    train_labels=reshape(train_labels',N*K,1);
     
-    test_labels=ones(8,91).*[1:8]';
-    test_labels=reshape(test_labels',728,1);
+    test_labels=ones(K,91).*[1:K]';
+    test_labels=reshape(test_labels',K*91,1);
     
-    clean_labels=ones(8,L).*[1:8]';
-    clean_labels=reshape(clean_labels',L*8,1);
+    clean_labels=ones(K,L).*[1:K]';
+    clean_labels=reshape(clean_labels',L*K,1);
     
     
     rng(0)
@@ -41,7 +41,7 @@ function [clean_data,shift_data,pp_test]=shift(index,train_trial,Xplan)
     shift_data=combined_count*coeff;
     shift_data(:,end+1)=train_labels;
     
-    pp_test=Xplan*coeff;
+    pp_test=Xplan(1:K*91,:)*coeff;
     pp_test(:,end+1)=test_labels;
     
 end
